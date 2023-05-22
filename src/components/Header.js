8784b12051d1d4d529d83e846bff4dd4ce741e1e
sreehari789 <sreeharipraj3693@gmail.com>
2023-05-22 18:10:09 +0530

    logout updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Header() {
 
   const uname= localStorage.getItem('uName')
+  const navigate= useNavigate()
+
+  const handleLogout=()=>{
+    localStorage.removeItem('uId')
+    localStorage.removeItem('uName')
+    navigate('/')
+  }
 
   return (
     <div> 
         <Navbar className='navbar'>
     <Container>
-    <i class="fa-solid fa-people-roof"></i>
-    &#160;   &#160;
-      <Navbar.Brand  className='text-light' href="/home">SupplierTracker</Navbar.Brand>
+  <Link to={'home'}>
+      <i class="fa-solid fa-people-roof"></i>
+      &#160;   &#160;
+        <Navbar.Brand  className='text-light'>SupplierTracker</Navbar.Brand>
+  </Link>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text  className='text-light'>
           Signed in as: <span className='text-primary'>{uname}</span>
         </Navbar.Text>
         &#160;   &#160;
-        <button className='m-1 bg-danger text-light'>Log Out</button>
+        <button onClick={handleLogout} className='m-1 bg-danger text-light'>Log Out</button>
       </Navbar.Collapse>
     </Container>
   </Navbar>
